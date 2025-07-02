4c6efa2cf50620ca82241b842c8a1f1d50c8fd43
Shadman Sakib <shadmanabir@outlook.com>
2025-07-02 10:52:05 +0600

    refactor: consolidate login screen and update routing

--- app/auth/_layout.tsx
+++ app/auth/_layout.tsx
@@ -14,7 +14,7 @@ export default function AuthLayout() {
 
   return (
     <Stack>
-      <Stack.Screen name="login/index" options={{ headerShown: false }} />
+      <Stack.Screen name="login" options={{ headerShown: false }} />
     </Stack>
   );
 }
